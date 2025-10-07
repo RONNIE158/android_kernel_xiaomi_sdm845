7f2198e7b387f26f409458c240fd794101ff49d9
beluga <ronnie158@live.com>
2025-10-07 12:35:49 +0700

    Create kgsl_vnc_display.c

---
+++ drivers/gpu/kgsl-vnc/kgsl_vnc_display.c
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * KGSL Virtual Display Stream via VNC (Dummy)
+ * Author: Bro Project
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/kthread.h>
+#include <linux/net.h>
+#include <linux/in.h>
+#include <linux/slab.h>
+#include <linux/delay.h>
+#include <linux/kgsl.h>
+#include <linux/mm.h>
+
+#define VNC_PORT 5900
+
+static struct socket *vnc_sock;
+static struct task_struct *vnc_thread;
+static int vnc_running = 1;
+
+static int vnc_stream_thread(void *data)
+{
+    struct sockaddr_in saddr;
+    int ret;
+
+    ret = sock_create(AF_INET, SOCK_STREAM, IPPROTO_TCP, &vnc_sock);
+    if (ret < 0) {
+        pr_err("kgsl_vnc: socket create failed\n");
+        return ret;
+    }
+
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    saddr.sin_port = htons(VNC_PORT);
+
+    ret = vnc_sock->ops->bind(vnc_sock, (struct sockaddr *)&saddr, sizeof(saddr));
+    if (ret < 0) {
+        pr_err("kgsl_vnc: bind failed\n");
+        goto out;
+    }
+
+    ret = vnc_sock->ops->listen(vnc_sock, 1);
+    if (ret < 0) {
+        pr_err("kgsl_vnc: listen failed\n");
+        goto out;
+    }
+
+    pr_info("kgsl_vnc: waiting for client on port %d...\n", VNC_PORT);
+
+    while (vnc_running) {
+        struct socket *client = NULL;
+        ret = vnc_sock->ops->accept(vnc_sock, &client, O_NONBLOCK);
+        if (ret == 0 && client) {
+            pr_info("kgsl_vnc: client connected\n");
+
+            while (vnc_running) {
+                char fb_data[1024];
+                memset(fb_data, 0x55, sizeof(fb_data)); // dummy frame
+                kernel_sendmsg(client, &(struct msghdr){},
+                               (struct kvec[]){{.iov_base = fb_data, .iov_len = sizeof(fb_data)}},
+                               1, sizeof(fb_data));
+                msleep(33);
+            }
+            sock_release(client);
+        }
+        msleep(100);
+    }
+
+out:
+    sock_release(vnc_sock);
+    return 0;
+}
+
+static int __init kgsl_vnc_init(void)
+{
+    pr_info("kgsl_vnc: init\n");
+    vnc_thread = kthread_run(vnc_stream_thread, NULL, "kgsl_vnc_stream");
+    if (IS_ERR(vnc_thread))
+        return PTR_ERR(vnc_thread);
+    return 0;
+}
+
+static void __exit kgsl_vnc_exit(void)
+{
+    vnc_running = 0;
+    if (vnc_thread)
+        kthread_stop(vnc_thread);
+    pr_info("kgsl_vnc: exit\n");
+}
+
+module_init(kgsl_vnc_init);
+module_exit(kgsl_vnc_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("KGSL Virtual Display Stream via VNC (Dummy)");
+MODULE_AUTHOR("Bro Project");
